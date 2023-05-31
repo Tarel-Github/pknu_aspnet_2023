83e2db9cb0765da659b0f4b3831997a86edb23d8
Tarel-Github <sms_stmax@naver.com>
2023-05-31 17:30:53 +0900

    포트폴리오

---
+++ Portfolio/PortfolioWebApp/Portfolio/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Portfolio.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Data
+{
+    public class ApplicationDbContext : IdentityDbContext   // 결국 DbContext 쓰는것하고 동일
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Board> Boards { get; set; }
+
+        // 포트폴리오를 DB로 관리하기 위한 모델
+        public DbSet<PortfolioModel> Portfolios { get; set; }
+
+        // 포트폴리오를 DB로 관리하기 위한 모델
+        public DbSet<TempPortfolioModel>? TempPortfolio { get; set; }
+    }
+}
---
+++ Portfolio/PortfolioWebApp/Portfolio/Models/Board.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Portfolio.Models
+{
+    public class Board
+    {
+        [Key] // PK
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "아이디를 입력하세요")] // Not Null
+        [DisplayName("아이디")]
+        public string UserId { get; set; }
+
+
+        [DisplayName("이름")]
+        public string? UserName { get; set; } // Null 허용
+
+
+        [Required(ErrorMessage = "제목을 입력하세요")] // Not Null
+        [MaxLength(200)] // == Varchar(200)
+        [DisplayName("제목")]
+        public string Title { get; set; }
+
+
+        [DisplayName("조회")]
+        public int ReadCount { get; set; }
+
+
+        [DisplayName("작성일")]
+        public DateTime PostDate { get; set; }
+
+
+        [DisplayName("게시글")]
+        public string Contents { get; set; }
+        
+    }
+}
